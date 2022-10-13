bab8412224d9ee7625ee5e2718a3abed38e4889c
musaliyah <asmusaliar@gmail.com>
2022-10-13 14:57:54 -0700

    working on routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,46 +2,21 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// Route "/"
+router.get('/', (req, res) => {
+  console.log(req.session);
 
-// Route "/login"
-
-// TODO: Add a comment describing the functionality of the withAuth middleware
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] }
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      // TODO: Add a comment describing the functionality of this property
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
-  // TODO: Add a comment describing the functionality of this if statement
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
-});
-
-module.exports = router;
-
-
-// Route "/dashboard"
-
-// Route "/dashboard/new"
-
-// Route "/dashboard/edit/:id"
-
-// Route "/post/:id"
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'created_at',
+      'post_content'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id, ']
+      }
+    ]
+  })
+})
