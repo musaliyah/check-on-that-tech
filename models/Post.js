dfe56fad72f0e87d0172bb90cf4d81871f93d76e
musaliyah <asmusaliar@gmail.com>
2022-10-16 15:39:29 -0700

    post model

---
+++ models/Post.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequalize');
+const sequalize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false, 
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    post_content: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        reference: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+},
+{
+    sequalize, 
+    freeTableName: true,
+    underscored, true, 
+    modelName: 'post'
+})
