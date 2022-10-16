9d947eaab68639d08f732b74829bd28f59d5c21a
musaliyah <asmusaliar@gmail.com>
2022-10-16 15:13:21 -0700

    post-routes drafting

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,4 +3,72 @@ const { Post, Comment, User } = require('../../models');
 const sequalize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
-router.get('/')
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'created_at', 'user_id', 'post_id'],
+                include: {
+                    model: User, 
+                    attributes: ['username', 'github', 'twitter']
+                }
+            },
+            {
+                model: User, 
+                attributes: ['username', 'github', 'twitter']
+            },
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username', 'github', 'twitter']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'post_id', 'user_id', 'comment', 'created_at'],
+                include: {
+                    model: User, 
+                    attributes: ['username', 'github', 'twitter']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post in our system matches this id'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
